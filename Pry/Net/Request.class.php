21d0992b3db75fe92bc5c0d89f2cac4bd1ca3a87
Olivier <Olivier@OlivierR>
2013-01-10 15:54:35 +0100

    Adding a fallback for getallheaders in case of php < 5.4 and fast cgi installation

--- Pry/Net/Request.class.php
+++ Pry/Net/Request.class.php
@@ -60,7 +60,7 @@ class Request
     public function getHeaders()
     {
         if (empty($this->headers))
-            $this->headers = getallheaders();
+            $this->headers = $this->getAllHeaders();
 
         return $this->headers;
     }
@@ -363,6 +363,26 @@ class Request
         else
         {
             return null;
+        }
+    }
+    
+    private function getAllHeaders()
+    {
+        if (!function_exists('getallheaders')) 
+        { 
+            $headers = ''; 
+            foreach ($_SERVER as $name => $value) 
+            { 
+                if (substr($name, 0, 5) == 'HTTP_') 
+                { 
+                    $headers[str_replace(' ', '-', ucwords(strtolower(str_replace('_', ' ', substr($name, 5)))))] = $value; 
+                } 
+            } 
+            return $headers; 
+        }
+        else
+        {
+            return getallheaders();
         }
     }
 
