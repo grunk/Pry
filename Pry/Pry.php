8a3613c721c8dcbcdd4a052f04a9d3f523e7a88b
oroger <oroger@prynel.com>
2018-01-03 10:23:41 +0100

    Rename all class to be psr-4 compliant

--- Pry/Pry.php
+++ Pry/Pry.php
@@ -65,7 +65,7 @@ class Pry
         // replace the namespace prefix with the base directory, replace namespace
         // separators with directory separators in the relative class name, append
         // with .php
-        $file = $base_dir . str_replace('\\', '/', $relative_class) . '.class.php';
+        $file = $base_dir . str_replace('\\', '/', $relative_class) . '.php';
         // if the file exists, require it
         if (file_exists($file)) {
             require $file;
